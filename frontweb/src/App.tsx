fcd90384f03cea0d0ba0caf7ef3cb2a4a207995b
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-01-24 22:44:01 +0100

    Home styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,12 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
-import Navbar from './components/Navbar';
+import Home from 'pages/Home';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <h1>Olá DSCatalog</h1>
+      <Home />
     </>
   );
 };
---
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+const ButtonIcon = () => {
+  return (
+    <button className="btn btn-primary btn-icon">
+      <h6>Inicie agora a sua busca</h6>
+    </button>
+  );
+};
+
+export default ButtonIcon;
