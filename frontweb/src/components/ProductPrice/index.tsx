8c11b6307fe36e83c9c6bd78846d9e5f12daf6b4
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-01-26 05:28:29 +0100

    Routes added

---
+++ frontweb/src/components/ProductPrice/index.tsx
@@ -0,0 +1,12 @@
+import './styles.css';
+
+const ProductPrice = () => {
+  return (
+    <div className="product-price-container">
+      <span>R$</span>
+      <h3>9.999.99</h3>
+    </div>
+  );
+};
+
+export default ProductPrice;
---
+++ frontweb/src/pages/Admin/index.tsx
@@ -0,0 +1,5 @@
+const Admin = () => {
+  return <h1>Página ADMIN</h1>;
+};
+
+export default Admin;
