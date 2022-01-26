153407179096b2df50ec3eac44a7726e65a76d01
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-01-26 06:09:20 +0100

    ButtonIcon customization

--- frontweb/src/pages/Home/index.tsx
+++ frontweb/src/pages/Home/index.tsx
@@ -10,15 +10,17 @@ const Home = () => {
       <div className="base-card home-card">
         <div className="home-content-container">
           <div>
-            <h1>Conheça o melhor catalago de produtos</h1>
+            <h1>Conheça o melhor catálogo de produtos</h1>
             <p>
               Ajudaremos você a encontrar os melhores produtos disponíveis no
               mercado.
             </p>
           </div>
-          <Link to="/products">
-            <ButtonIcon />
-          </Link>
+          <div>
+            <Link to="/products">
+              <ButtonIcon />
+            </Link>
+          </div>
         </div>
         <div className="home-image-container">
           <MainImage />
