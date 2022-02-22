488781063ae3fcb46f0002770eed6f1d6853c885
Alexandre Mathias <mathias.alexandre@gmail.com>
2022-02-22 17:19:43 +0100

    DTO added

--- backend/src/main/java/com/axnd/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/axnd/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.axnd.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.axnd.dscatalog.dto.CategoryDTO;
 import com.axnd.dscatalog.entities.Category;
 import com.axnd.dscatalog.repositories.CategoryRepository;
 
@@ -16,10 +18,10 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list =  repository.findAll();	
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
 		
 	}
-	
 	
 }
